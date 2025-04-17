cd412aa3c8389ab854ff740c31e465c90a86c33c
kundanYadav1 <ny88119@gmail.com>
2025-04-17 19:10:19 +0530

    ..

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./css/App.css";
 import Favorite from "./pages/Favorites";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
-import NavBar from "./components/navBar";
+import NavBar from "./components/NavBar";
 import { MovieProvider } from "./contexts/MovieContext";
 
 
